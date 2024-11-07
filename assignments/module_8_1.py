cdd7f763461e32d96305319c9c8a8a66dea76655
fam-ca <tramanh18111998@gmail.com>
2024-11-07 10:48:26 +0300

    commit

--- assignments/module_8_1.py
+++ assignments/module_8_1.py
@@ -1,9 +1,9 @@
 def add_everything_up(a, b):
     try:
-        print(a+b)
+        return a+b
     except:
-        print(str(a)+ str(b))
+        return str(a)+ str(b)
 
-add_everything_up(123.456, 'строка')
-add_everything_up('яблоко', 4215)
-add_everything_up(123.456, 7)
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
