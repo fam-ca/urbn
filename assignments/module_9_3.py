7138cb53acc3f92f38134092127f1f7646aa10d3
fam-ca <tramanh18111998@gmail.com>
2024-11-09 15:42:10 +0300

    commit

--- assignments/module_9_3.py
+++ assignments/module_9_3.py
@@ -1,1 +1,13 @@
-# генераторы
+# генераторы (в них выполняются ленивые вычисления)
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x)!=len(y))
+print(list(first_result))
+
+
+second_result = (len(first[i])==len(second[i]) for i in range(len(first)))
+print(list(second_result))
+
+# print(list(second_result)) # второй раз не выполнится
