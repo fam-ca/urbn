8df1ebf605eba34fe293ced77d1c53728ed64524
fam-ca <tramanh18111998@gmail.com>
2024-11-09 15:15:52 +0300

    commit

--- assignments/module_9_2.py
+++ assignments/module_9_2.py
@@ -10,6 +10,6 @@ print(first_result)
 second_result = [(x, y) for x in first_strings for y in second_strings if len(x)==len(y)]
 print(second_result)
 
-third_result = dict([(x, len(x)) for x in first_strings+second_strings if len(x)%2==0])
+third_result = {x: len(x) for x in first_strings+second_strings if len(x)%2==0}
 print(third_result)
 
