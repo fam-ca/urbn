eed44f4745224a469b40cc635d63fcb203ae4d77
fam-ca <tramanh18111998@gmail.com>
2024-11-09 14:43:56 +0300

    commit

--- assignments/module_9_1.py
+++ assignments/module_9_1.py
@@ -8,5 +8,7 @@ def apply_all_func(int_list, *functions):
     return results
 
 
-print(apply_all_func(['d', 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+print(apply_all_func(['svf', 20, 15, 9], len, sum, sorted))
