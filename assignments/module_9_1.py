2faf0602a2dd6edfeb43dea991537b38ec300fad
fam-ca <tramanh18111998@gmail.com>
2024-11-09 14:43:02 +0300

    commit

--- assignments/module_9_1.py
+++ assignments/module_9_1.py
@@ -1,8 +1,12 @@
 def apply_all_func(int_list, *functions):
     results = {}
-    for function in functions:
-        results[function.__name__] = function(int_list)
+    try:
+        for function in functions:
+            results[function.__name__] = function(int_list)
+    except TypeError:
+        return f'Некорректный тип данных'
     return results
 
-print(apply_all_func([6, 20, 15, 9], max, min))
+
+print(apply_all_func(['d', 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
