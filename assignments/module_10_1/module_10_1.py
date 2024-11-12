f3a787905944d9b1c2ba338936c0c41a8382afff
fam-ca <tramanh18111998@gmail.com>
2024-11-12 23:45:05 +0300

    commit

---
+++ assignments/module_10_1/module_10_1.py
@@ -0,0 +1,41 @@
+import threading
+import time
+import os
+
+os.chdir('assignments/module_10_1')
+print(os.getcwd())
+
+def write_words(word_count, file_name):
+    start = time.time()
+    with open(file_name, 'w') as file:
+        for i in range(word_count):
+            file.write('Какое-то слово № ' + str(i+1) + '\n')
+            time.sleep(0.1)
+    stop = time.time()
+    print(f'Завершилась запись в файл {file_name}')
+    return stop-start
+
+start1 = time.time()
+word_counts = [10, 30, 200, 100]
+file_names = ['example1.txt', 'example2.txt', 'example3.txt', 'example4.txt']
+for i in range(4):
+    write_words(word_counts[i], file_names[i])
+stop1 = time.time()
+print(f'Работа потоков: {(stop1-start1)} с.')
+
+
+# работа с потоками
+file_names = ['example5.txt', 'example6.txt', 'example7.txt', 'example8.txt']
+start2 = time.time()
+
+
+threads = [threading.Thread(target=write_words, args=(word_counts[i], file_names[i])) for i in range(4)]
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+stop2 = time.time()
+print(f'Работа потоков: {(stop2-start2)} с.')
--- vebinars/module9_0811.py
+++ vebinars/module9_0811.py
@@ -1,3 +1,40 @@
+# список, строки - итерируемые объекты, внутри итерируемого объекта есть итератор __iter__, __next__
+# итератор
+# next - метод для получения следующего значения итератора
+# у каждого итерируемого объекта есть функция next
+# но применить next можно только к итератору
+# next(list_) # не сработает!
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+iterator = iter(lst) # получение итератора
+
+class Humans:
+    def __init__(self, group):
+        self.group = group
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.counter < len(self.group):
+            first = self.group[self.counter]
+            self.counter += 1
+            return first
+        else:
+            raise StopIteration
+
+students = Humans(['Alice', 'Bob', 'Charlie'])
+print(iter(students))
+
+for i in students:
+    print(i)
+
+'''по-хорошему нужно реализовать отдельный класс для итератора и убрать __next__ 
+внутри Humans, оставив только __iter__
+Итерируемый объект - это тот, у кого есть итератор.
+А внутри итератор определна логика, что мы будем передавать последующим элементом
+'''
+
 def choice_func(choice):
     x = 2
     y = 3
@@ -10,9 +47,6 @@ def choice_func(choice):
 
 # print(choice_func(1))
 
-
-lst = [1, 2, 3, 4, 5, 6, 7, 8]
-iterator = iter(lst)
 
 # map
 list_square = list(map(lambda x: x**2, lst))
@@ -27,12 +61,18 @@ from functools import reduce
 total = reduce(lambda x, y: x+y, lst)
 print(total)
 
-# генераторные выражения способ создания итерируемого объекта, не загружая память
+'''
+генераторные выражения способ создания итерируемого объекта, не загружая память
+генератор помнит только о текущем элементе, который будут передавать
+генератор - частное решение итератора 
+'''
+
 squares = (x * x for x in range(10))
 print(next(squares))
 print(next(squares))
 
 # генераторные последовательности
+# yield похож на return, но разница в том, что после yield функция продолжит работать
 def fib():
     a, b = 0, 1
     while True:
