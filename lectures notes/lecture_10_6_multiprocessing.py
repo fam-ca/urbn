f93051889e052517eabfbf24c9df577f1b675147
fam-ca <tramanh18111998@gmail.com>
2024-12-02 14:30:57 +0300

    commit

--- lectures notes/lecture_10_6_multiprocessing.py
+++ lectures notes/lecture_10_6_multiprocessing.py
@@ -1,27 +1,46 @@
-import multiprocessing
-import time
+# import multiprocessing
+# import time
+# import threading
+
+# counter = 0
+
+
+# def first_worker(n):
+#     global counter
+#     for i in range(n):
+#         counter += 1
+#         time.sleep(1)
+#     print('First worker changed the counter to ', counter)
+
+
+# def second_worker(n):
+#     global counter
+#     for i in range(n):
+#         counter += 1
+#         time.sleep(1)
+#     print('Second worker changed the counter to ', counter)
+
+# if __name__== '__main__':
+#     process1 = multiprocessing.Process(target=first_worker, args=(10,))
+#     process2 = multiprocessing.Process(target=second_worker, args=(15,))
+#     process1.start()
+#     process2.start()
+
 import threading
 
 counter = 0
 
-
-def first_worker(n):
+def increment():
     global counter
-    for i in range(n):
+    for _ in range(100000):
         counter += 1
-        time.sleep(1)
-    print('First worker changed the counter to ', counter)
 
+threads = [threading.Thread(target=increment) for _ in range(2)]
 
-def second_worker(n):
-    global counter
-    for i in range(n):
-        counter += 1
-        time.sleep(1)
-    print('Second worker changed the counter to ', counter)
+for t in threads:
+    t.start()
 
-if __name__== '__main__':
-    process1 = multiprocessing.Process(target=first_worker, args=(10,))
-    process2 = multiprocessing.Process(target=second_worker, args=(15,))
-    process1.start()
-    process2.start()
+for t in threads:
+    t.join()
+
+print(counter)
