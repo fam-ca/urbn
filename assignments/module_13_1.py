46ad4c7445a28c6a1ec44fd165857739c796db40
fam-ca <tramanh18111998@gmail.com>
2024-12-24 20:40:43 +0300

    module_13_1

---
+++ assignments/module_13_1.py
@@ -0,0 +1,21 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    total_number = 5
+    print(f'Силач {name} начал соревнования.')
+    for i in range(total_number):
+        print(f'Силач {name} поднял {i+1} шар.')
+        await asyncio.sleep(power)
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    task_for_silach_1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task_for_silach_2 = asyncio.create_task(start_strongman('Denis', 4))
+    task_for_silach_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task_for_silach_1
+    await task_for_silach_2
+    await task_for_silach_3
+
+if __name__ == "__main__":
+    asyncio.run(start_tournament())
