3bf20cb666b1ac9d2deb545bbd713c3b4b115b5b
fam-ca <tramanh18111998@gmail.com>
2024-12-26 18:13:29 +0300

    commit

---
+++ assignments/module_13/module_13_3.py
@@ -0,0 +1,25 @@
+# module 13_2
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "" # insert your token here
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# handler
+@dp.message_handler(commands=["start"])
+async def start(message):
+    print("Привет! Я бот, помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.")
+
+@dp.message_handler(text=["Привет", "Здравствуй, бот"]) # перехват 2 сообщений
+async def urban_message(message):
+    print("Привет!")
+
+@dp.message_handler() # перехват всех сообщений
+async def all_messages(message):
+    print("Введите команду /start, чтобы начать сообщение.")
+    await message.answer("Введите команду /start, чтобы начать сообщение.")
+if __name__=="__main__":
+    executor.start_polling(dp, skip_updates=True)
