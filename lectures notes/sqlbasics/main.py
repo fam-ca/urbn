6be30a450524b5198fb0090e7d2faa9d0fa37064
fam-ca <tramanh18111998@gmail.com>
2025-01-16 15:41:44 +0300

    commit

---
+++ lectures notes/sqlbasics/main.py
@@ -0,0 +1,37 @@
+import sqlite3
+from random import randint
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+# # создание элементов
+# for i in range(30):
+#     cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", (f"newuser{i}", f"{i}example@gmail.com", str(randint(20, 60))))
+
+# # обновление элементов
+# cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
+
+# # удаление элементов
+# cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
+
+# # считывать данные
+# cursor.execute("SELECT * FROM Users")
+
+# cursor.execute("SELECT username, age FROM Users WHERE age > ?", (29, ))
+
+cursor.execute("SELECT username, age FROM Users GROUP BY age")
+users = cursor.fetchall()
+for user in users:
+    print(user)
+connection.commit()
+connection.close()
+
